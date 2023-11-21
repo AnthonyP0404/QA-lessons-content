28f0f5f4cc08e70f0cc12e35f1ae92058ec5d3e2
AnthonyP0404 <anthony.peterson00@gmail.com>
2023-11-21 16:32:31 +0000

    finished substring and touched on ascii

--- JavaScript/sandbox/challenge1.js
+++
@@ -1,7 +0,0 @@
-//add both digits of the number together
-
-let A=57
-console.log(parseInt(A/10) + (A%10))
-
-
-
--- JavaScript/sandbox/challenge2.js
+++
@@ -1,5 +0,0 @@
-//add all 3 digits of the number together
-
-let B = 345
-// console.log(parseInt(B/100) + parseInt((B-parseInt(B/100)*100)/10) + ((B-parseInt(B/100)*100)%10))    //compicated it too much
-console.log(parseInt(B/100) + parseInt((B%100)/10) + parseInt((B%100)%10))    //this is the simplified
