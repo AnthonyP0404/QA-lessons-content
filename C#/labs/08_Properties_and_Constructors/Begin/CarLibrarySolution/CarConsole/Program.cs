f1967bf94f0ad124826a50c3b81302b478a3c59e
Anthony Peterson <114401905+AnthonyP0404@users.noreply.github.com>
2023-12-21 13:54:28 +0000

    uploaded lab 8

---
+++ C#/labs/08_Properties_and_Constructors/Begin/CarLibrarySolution/CarConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CarLibrary;
+using System.Security.AccessControl;
+
+Car c1 = new Car();
+Console.WriteLine(nameof(c1));
+c1.Make = "Ford";
+Console.WriteLine(c1.Make);
+Console.WriteLine(c1.Model);
+
+Console.WriteLine("");
+
+Car c2 = new Car("Audi", "TT");
+Console.WriteLine(nameof(c2));
+Console.WriteLine(c2.Make);
+Console.WriteLine(c2.Model);
+c2.Colour = "Red";
+Console.WriteLine(c2.Colour);
+c2.Speed = 30;
+Console.WriteLine(c2.Speed + "mph");
+Console.WriteLine(c2.SpeedInKilometers + "kph");
+
+Console.WriteLine("");
+
+Car c3 = new Car("BMW", "X5") { Colour = "Grey", RegistrationNumber = "ABC 123" };
+Console.WriteLine($"{nameof(c3)}");
+Console.WriteLine(c3.Make);
+Console.WriteLine(c3.Model);
+Console.WriteLine(c3.Colour);
+Console.WriteLine(c3.RegistrationNumber);
+
+Console.WriteLine("");
+
+Car c4 = new Car();
+Console.WriteLine(nameof(c4));
+Console.WriteLine(c4.Make);
+Console.WriteLine(c4.Model);
+Console.WriteLine(c4.Colour);
+Console.WriteLine(c4.RegistrationNumber);
---
+++ C#/labs/08_Properties_and_Constructors/Begin/CarLibrarySolution/CarLibrary/Car.cs
@@ -0,0 +1,38 @@
+﻿namespace CarLibrary
+{
+    public class Car
+    {
+        // FIELDS
+        private int speed;
+        public int Speed
+        {
+            get { return speed; }
+            set { if(value > 0 && value < 100) speed = value; }
+        }
+
+        public string RegistrationNumber { get; set; }
+
+        public double SpeedInKilometers => Speed * 1.609344;
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Colour { get; set; }
+
+
+
+        // CONSTRUCTORS
+        public Car(string Make, string Model)
+        {
+            this.Make = Make;
+            this.Model = Model;
+        }
+        public Car() :this("Unknown Make", "Unknown Model")
+        {
+            // this.Make = "Unknown";
+            // this.Model = "Unknown";
+            //this.Colour = "Black";
+
+            this.Colour = "White";
+        }
+    }
+}
